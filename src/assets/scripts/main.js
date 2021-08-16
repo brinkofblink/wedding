1d246aa7ca915e57668d25c100a60293dda9d2a8
Tom Haskins <tom@so-design.co.uk>
2021-08-16 14:41:50 +0100

    update form to use id

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,3 +3,17 @@ import 'focus-visible'
 
 // Internal Modules
 import './modules/sunset'
+
+let savedId = localStorage.getItem('list-subscriber-id');
+if (!savedId) {
+  let params = new URLSearchParams(document.location.search.substring(1));
+  let id = params.get("id")
+  if (id) {
+    localStorage.setItem('list-subscriber-id', id);
+    savedId = id
+  }
+}
+const idElement = document.querySelector('[data-tf-hidden]')
+if (savedId && idElement) {
+  idElement.dataset.tfHidden = `id=${savedId}`
+}
